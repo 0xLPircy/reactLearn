714736c9b36e66933441198ca15271f6766025ef
L-Pircy <taveesha.agarwal@gmail.com>
2023-05-21 13:02:03 +0530

    step-form, userform, next/prev and field change methods

---
+++ react/react_step_form/src/Components/UserForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
+    }
+
+    // Proceed to next step
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        });
+    }
+
+    // Proceed to prev step
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1
+        });
+    }
+
+    // Handle fields change
+    handleChange = input => e => {
+        this.setState({ [input]: e.target.value });
+    }
+
+    render() {
+        return (
+            <div>UserForm</div>
+        )
+    }
+}
+
+export default UserForm
