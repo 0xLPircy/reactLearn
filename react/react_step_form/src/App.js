c2b4513dad64c98f0fd485b46127a87116690e2c
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-08 20:41:43 +0530

    tried solving the react tailwind version issue

--- react/react_step_form/src/App.js
+++ react/react_step_form/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { UserForm } from './Components/UserForm';
+import UserForm from './Components/UserForm';
 
 function App() {
   return (
--- react/react_step_form/src/Components/FormUserDetails.js
+++ react/react_step_form/src/Components/FormUserDetails.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar'; //for navbar looks
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/raisedButton';
+import { ThemeProvider } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar'; //for navbar looks
+import TextField from '@mui/material/TextField';
+import RaisedButton from '@mui/material/Button';
 
 
 export class FormUserDetails extends Component {
     continue = e => {
         e.preventDefault();
-        this.props.nextStep
+        this.props.nextStep();
     }
     render() {
         const { values } = this.props;
-        this.props
         return (
-            <MuiThemeProvider>
-                <h1>Hello from user details</h1>
-            </MuiThemeProvider>
+            <ThemeProvider>
+                <React.Fragment>
+
+                </React.Fragment>
+            </ThemeProvider>
         )
     }
 }
